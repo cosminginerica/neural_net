b7ab221943e4e017227e2a90dae79c166d820edb
cosminginerica <cosmin.ginerica@gmail.com>
2016-10-16 00:02:02 +0300

    Added MaxPoolLayer

--- include/MaxPoolLayer.h
+++ include/MaxPoolLayer.h
@@ -1,9 +1,27 @@
-#ifndef MAXPOOLLAYER_H
-#define MAXPOOLLAYER_H
-
+#ifndef MAXPOOLINGLAYER_H
+#define MAXPOOLINGLAYER_H
+#include "NeuronLayer.h"
+#include "Types.h"
+#include <assert.h>
 class MaxPoolLayer
 {
+public:
+	MaxPoolingLayer(const unsigned maxPoolSize, int activation) : maxPoolSize(maxPoolSize){}
+	MaxPoolingLayer(){}
+	void calculateOutputs();
+	FloatingType** getOutputs();
+	const int getOutputSize();
+	void backPropagate(const int label);
+	FloatingType ** getWeights();
 
+	void init();
+	std::string const getClassId(){ return "MaxPool"; }
+	FloatingType **getDelta();
+	ostream& serialize(ostream& f)const;
+
+private:
+	unsigned maxPoolSize;
+	unsigned nrNeuronsPerPool;
+	NeuronLayer *outputLayer, *inputLayer;
 };
 #endif
-
--- src/MaxPoolLayer.cpp
+++ src/MaxPoolLayer.cpp
@@ -1,2 +1,131 @@
-#include "MaxPoolLayer.h"
+#include "MaxPoolingLayer.h"
+#include "FeatureMapLayer.h"
 
+void MaxPoolingLayer::calculateOutputs()
+{
+	FloatingType **prevOutputs = inputLayer->getOutputs();
+	FloatingType **prevZs = inputLayer->getZ();
+	int prevRows = inputLayer->getRows();
+	unsigned rows = prevRows / maxPoolSize;
+	int prevCols = inputLayer->getCols();
+	unsigned cols = prevCols / maxPoolSize;
+	for (unsigned i = 0; i < rows; i++)
+	{
+		for (unsigned j = 0; j < cols; j++)
+		{
+			FloatingType max = FLT_MIN;
+			FloatingType maxZ = max;
+			for (unsigned k = 0; k < maxPoolSize; ++k)
+			{
+				for (unsigned l = 0; l < maxPoolSize; ++l)
+				{
+					if (prevOutputs[i * maxPoolSize + k][j * maxPoolSize + l] > max)
+					{
+						max = prevOutputs[i * maxPoolSize + k][j * maxPoolSize + l];
+						maxZ = prevZs[i * maxPoolSize + k][j * maxPoolSize + l];
+					}
+				}
+			}
+			activations[i][j] = max;
+			zs[i][j] = maxZ;
+		}
+	}
+
+}
+
+FloatingType** MaxPoolingLayer::getOutputs()
+{
+	return activations;
+}
+
+const int MaxPoolingLayer::getOutputSize()
+{
+	return numberOfNeurons;
+}
+
+void MaxPoolingLayer::backPropagate(const int label)
+{
+	FloatingType **nextWeights;
+	nextWeights= outputLayer->getWeights();
+	FloatingType **nextDeltas = outputLayer->getDelta();
+	FloatingType *nextDeltasVec = HelperFunctions::mat2vec(nextDeltas, 
+	                                                       outputLayer->getRows(), 
+	                                                       outputLayer->getCols());
+	                                                       
+	FloatingType **w_t = HelperFunctions::matrixTranspose(nextWeights, 
+	                                                      outputLayer->getNumberOfNeurons(),
+	                                                      numberOfNeurons);
+	                                                      
+	FloatingType *w_d = HelperFunctions::matVecMul(w_t, 
+	                                               numberOfNeurons, 
+	                                               outputLayer->getNumberOfNeurons(), 
+	                                               nextDeltasVec, 
+	                                               outputLayer->getNumberOfNeurons());
+	
+	FloatingType **sigmoids = activation->activationPrimeMat(zs, rows, cols);
+	FloatingType *sigmoidsVec = HelperFunctions::mat2vec(sigmoids, rows, cols);
+	FloatingType *d = HelperFunctions::hadamardProduct(w_d, sigmoidsVec, numberOfNeurons);
+	unsigned crtIdx = 0;
+	for (unsigned i = 0; i < rows; ++i)
+	{
+		for (unsigned j = 0; j < cols; ++j)
+		{
+			deltas[i][j] = d[crtIdx++];
+		}
+	}
+	delete[]nextDeltasVec;
+	for (unsigned i = 0; i < rows; ++i)
+		delete[]sigmoids[i];
+	delete[]sigmoids;
+	delete[]sigmoidsVec;
+	delete[]d;
+	delete[]w_d;
+	for (int i = 0; i < numberOfNeurons; ++i)
+	{
+		delete[]w_t[i];
+	}
+	delete[]w_t;
+}
+
+void MaxPoolingLayer::init()
+{
+	assert(inputLayer->getCols() % maxPoolSize == 0 && inputLayer->getRows() % maxPoolSize == 0);
+	isOutput = false;
+	numberOfNeurons = inputLayer->getNumberOfNeurons() / (2 * maxPoolSize);
+	this->rows = inputLayer->getRows() / maxPoolSize;
+	this->cols = inputLayer->getCols() / maxPoolSize;
+	activations = new FloatingType*[rows];
+	zs = new FloatingType*[rows];
+	deltas = new FloatingType*[rows];
+	for (unsigned i = 0; i < rows; ++i)
+	{
+		activations[i] = new FloatingType[cols];
+		zs[i] = new FloatingType[cols];
+		deltas[i] = new FloatingType[cols];
+	}
+}
+
+FloatingType** MaxPoolingLayer::getDelta()
+{
+	return deltas;
+}
+
+ostream& MaxPoolingLayer::serialize(ostream& f)const
+{
+	f << "Class ID:" << this->classId.c_str() << std::endl;
+	f << "Number of neurons:" << this->numberOfNeurons << std::endl;
+	f << "Rows:" << this->rows << std::endl;
+	f << "Cols:" << this->cols << std::endl;
+	f << "Max pool size:" << this->maxPoolSize << std::endl;
+	f << "Deltas:" << std::endl;
+	for (unsigned i = 0; i < rows; ++i)
+	{
+		for (unsigned j = 0; j < cols; ++j)
+		{
+			f << deltas[i][j] << " ";
+		}
+		f << std::endl;
+	}
+	return f;
+
+}
